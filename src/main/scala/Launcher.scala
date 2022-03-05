c44c48ebe6643b5edd21ff3da4efa2250cb2ecb6
remyd95 <remyduijsens@gmail.com>
2022-03-05 11:39:59 +0100

    Extend the argument parser and change broker ip to broker id

--- src/main/scala/Launcher.scala
+++ src/main/scala/Launcher.scala
@@ -7,11 +7,11 @@ object Launcher {
   private var nodeList = Map[Int, String]()
   private val usage = """
   Usage: Launcher
-    --type    client / broker
-    --ID      num
+    --type    client or broker
+    --ID      ID of the client
 
     In case of client:
-      --EB    IP of edge broker
+      --BID   ID of edge broker
 
     In case of broker
       --nbrs  "num num ..."
@@ -47,19 +47,24 @@ object Launcher {
     var arglist = args.toList
     var node_type = ""
     var node_ID = 0
-    var IP_EB = ""
+    var broker_ID = 0
     var neighbours : List[String] = List()
 
     while(arglist.nonEmpty) {
       arglist match {
         case "--type" :: value :: tail =>
-          node_type = value
-          arglist = tail
+          if (value == "broker" || value== "client") {
+            node_type = value
+            arglist = tail
+          } else {
+            println("Undefined Node Type")
+            exit(1)
+          }
         case "--ID" :: value :: tail =>
           node_ID = value.toInt
           arglist = tail
-        case "--EB" :: value :: tail =>
-          IP_EB = value
+        case "--BID" :: value :: tail =>
+          broker_ID = value.toInt
           arglist = tail
         case "--nbrs" :: value :: tail =>
           neighbours = value.split(' ').toList
@@ -70,21 +75,22 @@ object Launcher {
       }
     }
 
-
     // Test argument parse
+    println("Succesfully initalized Launcher with the following start-up parameters:")
     println("Type: " + node_type)
     println("ID: " + node_ID)
-    println("IP: " + IP_EB)
-    println("Neighbours: " + neighbours)
+    if (node_type == "client") println("BID: " + broker_ID) else println("Neighbours: " + neighbours)
 
     // Parse and print node list
     parseNodeList()
+    println("\nSuccesfully parsed Node List:")
     println(getNodeList())
 
     // TODO Create a client or a broker based on type
     configureSocket()
     initializeNode()
     startNode()
+    println(s"\nSuccesfully initalized the Node $node_type $node_ID")
 
     // TODO Delete this loop when the actual nodes are initialized with tasks
     while(true) {
